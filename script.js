f931846af4ad468863f4eba7e52b636c5effa846
MazineEZZ <mazinewolf2007@gmail.com>
2024-03-03 00:07:14 +0100

    Finished the project

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+const passwdDiv = document.querySelector(".password");
 const passwdInput = document.querySelector("#password");
 const confirmPasswdInput = document.querySelector("#confirm-password");
 
 function doPasswordsMatch() {
-  if (passwdInput.value === confirmPasswdInput.value) {
-    passwdInput.classList.remove("invalid");
-    confirmPasswdInput.classList.remove("invalid");
-  } else {
+  if (passwdInput.value !== confirmPasswdInput.value || !passwdInput.value || !confirmPasswdInput.value) {
     passwdInput.classList.add("invalid");
     confirmPasswdInput.classList.add("invalid");
+    passwdDiv.classList.add("error");
+    return false;
+  } else {
+    passwdInput.classList.remove("invalid");
+    confirmPasswdInput.classList.remove("invalid");
+    passwdDiv.classList.remove("error");
+    return true;
   }
 }
 
-passwdInput.addEventListener("keydown", () => {
-  console.log(passwdInput.value);
-  console.log(confirmPasswdInput.value);
-  doPasswordsMatch();
-});
-confirmPasswdInput.addEventListener("keydown", () => doPasswordsMatch());
+passwdInput.addEventListener("keyup", () => doPasswordsMatch());
+confirmPasswdInput.addEventListener("keyup", () => doPasswordsMatch());
